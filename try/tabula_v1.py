ab6dfc07351cade6f047bd4336532a31a00ce88f
Shaney Car <shaneyc111@gmail.com>
2024-09-25 01:25:56 +0200

    del try :bento:

--- try/tabula_v1.py
+++
@@ -1,9 +0,0 @@
-import tabula
-import pandas as pd
-
-# Extraire le tableau du fichier PDF et le convertir en DataFrame
-df = tabula.read_pdf('01.pdf', pages=1, multiple_tables=True)
-
-# Afficher les résultats
-print(df)
-                              
